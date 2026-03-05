87100b34a270e6e2ad0552b5475bebfb5fb4d000
Shened <joao.psm98@gmail.com>
2026-03-05 20:03:02 +0000

    Angular + Supabase Installed

---
+++ ops/src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://ycnoqajqjzqveslwzjdx.supabase.co',
+    supabaseKey: 'sb_publishable_oONrft073VJR327xjOZOVA_D6Eyppmv'
+};
